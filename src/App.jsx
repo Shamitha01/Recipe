14cc2f711e677c8aafe3496800b2f9f0984590ae
Shamitha N <83489123+Shamitha01@users.noreply.github.com>
2025-06-08 05:14:10 +0000

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
+// src/App.js
+
 import Navbar from "./components/Navbar";
 import AppRoutes from "./router/AppRoutes";
 import { Toaster } from "react-hot-toast";
+
 export default function App() {
   return (
-    <>
+    // Make this the main container that fills the screen
+    <div className="flex flex-col min-h-screen bg-white text-black">
       <Navbar />
       <AppRoutes />
       <Toaster
-  position="top-center"
-  toastOptions={{
-    duration: 3000,
-    style: {
-      background: "#fff",
-      color: "#14b8a6", // brand teal
-      border: "1px solid #14b8a6",
-      fontWeight: "500",
-    },
-  }}
-/>
-    </>
+        position="top-center"
+        toastOptions={{
+          duration: 3000,
+          style: {
+            background: "#fff",
+            color: "#14b8a6",
+            border: "1px solid #14b8a6",
+            fontWeight: "500",
+          },
+        }}
+      />
+    </div>
   );
 }
